693449110b3bb9e50627babb125a79fef74c4c4b
Enrique Jimenez <ejimenez2292@gmail.com>
2026-07-28 21:11:57 -0700

    added music

---
+++ timeline.js
@@ -0,0 +1,31 @@
+const timeline = [
+
+    {
+        type: "chapter",
+        title: "The Beginning",
+        subtitle: "August 5, 2025",
+        duration: 4000
+    },
+
+    {
+        type: "image",
+        src: "images/01.jpeg",
+        caption: "The day our lives changed forever.",
+        duration: 7000
+    },
+
+    {
+        type: "image",
+        src: "images/02.jpeg",
+        caption: "So tiny. So perfect.",
+        duration: 6000
+    },
+
+    {
+        type: "video",
+        src: "videos/01.mov",
+        caption: "Your very first cries.",
+        pauseMusic: true
+    }
+
+];
